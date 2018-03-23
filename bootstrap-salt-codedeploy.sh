26d25fc3825563ebf6eeb05999ff48099bed0c18
Miguel.Lopez <malope30@asu.edu>
2018-03-22 22:26:14 -0700

    first commit

---
+++ bootstrap-salt-codedeploy.sh
@@ -0,0 +1,24 @@
+# Dynamically fetch region for EC2 in aws
+region=$(curl http://169.254.169.254/latest/meta-data/placement/availability-zone | sed ’s/.$//‘);
+# gr8 packaages
+sudo yum -y install ruby wget jq;
+# Install codedeploy https://aws.amazon.com/codedeploy/
+sudo cd /home/ec2-user;
+sudo wget https://aws-codedeploy-${region}.s3.amazonaws.com/latest/install;
+sudo chmod +x ./install;
+sudo ./install auto;
+sudo service codedeploy-agent start;
+# What you will name your VMs
+INSTANCE_PREFIX={YOUR-ID-HERE}
+INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id);
+# Change hostnames
+echo $INSTANCE_PREFIX-$INSTANCE_ID > /etc/hostname;
+sed -i -e '/^127.0.0.1/d' /etc/hosts; 
+sed -i -e '/^::1/d' /etc/hosts; 
+echo 127.0.0.1 $INSTANCE_PREFIX-$INSTANCE_ID >> /etc/hosts;
+# Install and bootstrap salt-minion to saltmaster
+SALT_MASTER_IP={IP-TO-SALT-MASTER-HERE}
+mkdir -p /etc/salt/; $INSTANCE_PREFIX-$INSTANCE_ID > /etc/salt/minion_id;
+sudo curl -o /tmp/bootstrap-salt.sh -L https://bootstrap.saltstack.com;
+sudo sh /tmp/bootstrap-salt.sh -i $INSTANCE_PREFIX-$INSTANCE_ID -A $SALT_MASTER_IP;
+sudo rm -f /tmp/bootstrap-salt.sh;
